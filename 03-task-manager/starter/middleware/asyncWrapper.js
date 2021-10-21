051ff4c194e45359cb834625101d4b2f7e9a48eb
jpeg25 <jairo25509@hotmail.com>
2021-10-21 08:33:57 -0500

    Catch in async wrapper corrected.

--- 03-task-manager/starter/middleware/asyncWrapper.js
+++ 03-task-manager/starter/middleware/asyncWrapper.js
@@ -2,7 +2,9 @@ const asyncWrapper = (controllerFunction) => {
   return async (req, res, next) => {
     try {
       await controllerFunction(req, res, next);
-    } catch (error) {}
+    } catch (error) {
+      next(error);
+    }
   };
 };
 
