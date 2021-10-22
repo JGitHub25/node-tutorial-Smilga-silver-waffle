fc461482b21aaa9e155194f66c520d7a157230a7
jpeg25 <jairo25509@hotmail.com>
2021-10-22 10:32:40 -0500

    Initial setup including server and db.

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+//Dependencies.
+require("dotenv").config();
+require("express-async-errors");
+
+//Imports.
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const NotFoundMiddleware = require("./middleware/not-found");
+const connectDB = require("./db/connect");
+
+//Express.
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Store API is live!</h1>");
+});
+
+app.use(NotFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
