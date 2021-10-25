9a6af72e5dfe9fc0fe21a776ba01acb8ce6509e1
jpeg25 <jairo25509@hotmail.com>
2021-10-25 10:40:37 -0500

    Query for 'company, featured, name'  using query parameters.

--- 04-store-api/starter/controllers/productsControllers.js
+++ 04-store-api/starter/controllers/productsControllers.js
@@ -1,8 +1,33 @@
+const ProductsModel = require("../models/productModel");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route." });
+  const products = await ProductsModel.find({ company: "caressa" });
+  res.status(200).json({
+    msg: "Products testing route.",
+    nbHits: products.length,
+    products,
+  });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products route." });
+  //Se extraen la propiedades que interesan.
+  const { featured, company, name } = req.query;
+  const queryObj = {};
+  //Se verifica si las propiedades se pasaron/existen.
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: `^${name}`, $options: "i" };
+  }
+  console.log(queryObj);
+  //No se pasa req.query directamente sino otro objeto.
+  const products = await ProductsModel.find(queryObj);
+  res
+    .status(200)
+    .json({ msg: "Products route.", nbHits: products.length, products });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
