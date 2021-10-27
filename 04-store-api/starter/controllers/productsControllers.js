1f305e0d8009675f3aeee33a910519c0f89d5724
jpeg25 <jairo25509@hotmail.com>
2021-10-27 15:28:37 -0500

    Sorting working.

--- 04-store-api/starter/controllers/productsControllers.js
+++ 04-store-api/starter/controllers/productsControllers.js
@@ -1,7 +1,8 @@
 const ProductsModel = require("../models/productModel");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await ProductsModel.find({}).sort("-name").limit(3);
+  const products = await ProductsModel.find({ company: "marcos" });
+  console.log(products);
   res.status(200).json({
     msg: "Products testing route.",
     nbHits: products.length,
@@ -10,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 const getAllProducts = async (req, res) => {
   //Se extraen la propiedades que interesan.
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObj = {};
   //Se verifica si las propiedades se pasaron/existen.
   if (featured) {
@@ -22,9 +23,22 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObj.name = { $regex: `^${name}`, $options: "i" };
   }
+  console.log("queryObject");
   console.log(queryObj);
   //No se pasa req.query directamente sino otro objeto.
-  const products = await ProductsModel.find(queryObj);
+  let result = ProductsModel.find(queryObj);
+
+  if (sort) {
+    const sortTerms = sort.split(",").join(" ");
+    console.log(`The terms to sort are: ${sortTerms}`);
+    result = result.sort(sortTerms);
+  } else {
+    //Default sort.
+    result = result.sort("-createdAt");
+  }
+  //Se puede aguantar hasta aquí la resolución del Query object (después de haberle aplicado .find() y .sort()).
+  const products = await result;
+
   res
     .status(200)
     .json({ msg: "Products route.", nbHits: products.length, products });
