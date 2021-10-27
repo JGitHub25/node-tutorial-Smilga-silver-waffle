e19784386ff8c767ae5cb3dc6ecdc507ae027337
jpeg25 <jairo25509@hotmail.com>
2021-10-27 16:35:35 -0500

    Pagination for query results added.

--- 04-store-api/starter/controllers/productsControllers.js
+++ 04-store-api/starter/controllers/productsControllers.js
@@ -1,8 +1,7 @@
 const ProductsModel = require("../models/productModel");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await ProductsModel.find({ company: "marcos" });
-  console.log(products);
+  const products = await ProductsModel.find({}).select("name price").limit(3);
   res.status(200).json({
     msg: "Products testing route.",
     nbHits: products.length,
@@ -11,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 const getAllProducts = async (req, res) => {
   //Se extraen la propiedades que interesan.
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, selectors } = req.query;
   const queryObj = {};
   //Se verifica si las propiedades se pasaron/existen.
   if (featured) {
@@ -36,6 +35,19 @@ const getAllProducts = async (req, res) => {
     //Default sort.
     result = result.sort("-createdAt");
   }
+
+  if (selectors) {
+    const selectionTerms = selectors.split(",").join(" ");
+    console.log(`The terms to select are: ${selectionTerms}`);
+    result = result.select(selectionTerms);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = Number(page - 1) * limit;
+  //Este 'result' viene de unq query con .find().
+  result = result.skip(skip).limit(limit);
+
   //Se puede aguantar hasta aquí la resolución del Query object (después de haberle aplicado .find() y .sort()).
   const products = await result;
 
