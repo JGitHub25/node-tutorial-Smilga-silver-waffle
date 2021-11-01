0445b3e1bb4066b17e300bbe23f2fe4f5eae7a30
jpeg25 <jairo25509@hotmail.com>
2021-11-01 18:16:04 -0500

    JWT functionality.

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,5 @@
 const CustomErrorAPI = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -7,16 +8,34 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomErrorAPI(`A username and password must be provided`, 400);
   }
+  const id = new Date().getDate();
 
-  res.send("Fake Login/Register/Signup Route");
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ message: "User created", token });
 };
 
 const accessDashboard = (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    message: `Hello, user`,
-    secret: `Here's your authorized data. Your lucky number is ${luckyNumber}`,
-  });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomErrorAPI("No valid token provided.", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    //Respond.
+    res.status(200).json({
+      message: `Hello, ${decoded.username}`,
+      secret: `Here's your authorized data. Your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomErrorAPI("Not authorized to access this route.", 401);
+  }
 };
 
 module.exports = { login, accessDashboard };
