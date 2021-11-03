e5e004d6a7027354ef0ecb29f6d2596df481d77c
jpeg25 <jairo25509@hotmail.com>
2021-11-03 15:57:21 -0500

    Create user in registration -Mongoose validation.-

--- 06-jobs-api/starter/controllers/authController.js
+++ 06-jobs-api/starter/controllers/authController.js
@@ -1,9 +1,14 @@
+const UserModel = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
 const register = async (req, res) => {
-  res.json("register");
+  /*Se puede validar con Mongoose como está o con lógica -if(!name||!email||!password)- como en proyectos anteiores. */
+  const user = await UserModel.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
-  res.json("login");
+  res.status(StatusCodes.CREATED).json("login");
 };
 
 module.exports = { register, login };
