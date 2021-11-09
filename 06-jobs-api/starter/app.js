6d04c0c1247b4199ea288bfc78330193db2f5210
jpeg25 <jairo25509@hotmail.com>
2021-11-09 18:27:05 -0500

    security packages set.

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//Security packages.
+const rateLimiter = require("express-rate-limiter");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+
 const express = require("express");
 const app = express();
 const authUserMiddleware = require("./middleware/authentication");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 
 //DB.
 const connectDB = require("./db/connect");
