d6e96e17a8f4a389e2018ede4afdce475e637ae9
jpeg25 <jairo25509@hotmail.com>
2021-11-25 11:10:58 -0500

    Upload image to cloud ready.

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -4,7 +4,7 @@ const CustomError = require("../errors");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
-const uploadImage = async (req, res) => {
+const uploadImageToLocalServer = async (req, res) => {
   //Check if there is a file in the request.
   if (!req.files) {
     throw new CustomError.BadRequestError("Please upload a file.");
@@ -34,6 +34,20 @@ const uploadImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+  //Por qué return en esta función. No creo que se necesite.
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = {
