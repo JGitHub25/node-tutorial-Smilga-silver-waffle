5b6a8e561488625bdaea934fbdfba909cb05514f
jpeg25 <jairo25509@hotmail.com>
2021-11-24 19:30:32 -0500

    uploadImage controller functionality ready.

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,20 @@
 const { StatusCodes } = require("http-status-codes");
+const path = require("path");
 
 const uploadImage = async (req, res) => {
-  res.send("Image uploaded");
+  const productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    `../public/uploads/${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  //Por qué return en esta función. No creo que se necesite.
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
