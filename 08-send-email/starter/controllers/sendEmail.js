5a37956b7fc34c50383751824167da9372cd7b4c
jpeg25 <jairo25509@hotmail.com>
2021-12-13 15:08:10 -0500

    Email functionality with SendGrid (complete).

--- 08-send-email/starter/controllers/sendEmail.js
+++ 08-send-email/starter/controllers/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 
-const sendEmail = async (req, res) => {
+const sendEmailEthereal = async (req, res) => {
   let testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport({
@@ -13,13 +14,26 @@ const sendEmail = async (req, res) => {
   });
 
   let info = await transporter.sendMail({
-    from: '"Jairo Espine 👻" <jairo25509@hotmail.com>', // sender address
+    from: '"Jairo Espinel 👻" <jairo25509@hotmail.com>', // sender address
     to: "bar@example.com, baz@example.com", // list of receivers
     subject: "Hello ✔", // Subject line
     html: "<b>Hello world?</b>", // html body
   });
 
   res.send({ info });
+};
+
+const sendEmail = async (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "jespine3@eafit.edu.co", // Change to your recipient
+    from: "jairo25509@hotmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  const info = await sgMail.send(msg);
+  res.json(info); //O como se quiera responder.
 };
 
 module.exports = sendEmail;
